22dc008085ec0316795f02b577af4e0ed8c7fd33
minhquang231097 <minhquang231097@gmail.com>
2023-08-17 16:35:37 +0700

    17.8.2023 updated

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,18 +1,18 @@
 import axios from 'axios'
 
-// Function to refresh the access token using the refresh token
-async function refreshAccessToken(): Promise<string> {
-  const user = JSON.parse(localStorage.getItem('user') as string)
-  const response = await axios.post('/refresh', { refreshToken: user.refreshToken })
-  const { accessToken } = response.data
-  return accessToken
-}
-
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1',
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 })
+
+// Function to refresh the access token using the refresh token
+async function refreshAccessToken(): Promise<string> {
+  const user = JSON.parse(localStorage.getItem('user') as string)
+  const response = await axios.post(`${http.defaults.baseURL}/auth/refresh`, { refreshToken: user.refreshToken })
+  const { accessToken } = response.data
+  return accessToken
+}
 
 http.interceptors.request.use(
   (config) => {
