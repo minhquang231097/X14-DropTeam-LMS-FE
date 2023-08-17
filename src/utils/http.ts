cb9394035b1d3720448d39785b00d28f74d96165
minhquang231097 <minhquang231097@gmail.com>
2023-08-17 16:27:01 +0700

    feat: 17.8.2023 updated

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,12 +1,50 @@
 import axios from 'axios'
 
-const getUser = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') as string) : ''
-const token = getUser.access_token
+// Function to refresh the access token using the refresh token
+async function refreshAccessToken(): Promise<string> {
+  const user = JSON.parse(localStorage.getItem('user') as string)
+  const response = await axios.post('/refresh', { refreshToken: user.refreshToken })
+  const { accessToken } = response.data
+  return accessToken
+}
 
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1',
   timeout: 30000,
-  headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+  headers: { 'Content-Type': 'application/json' },
 })
+
+http.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem('user') as string)
+    if (user) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${user.accessToken}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+http.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async (error) => {
+    const originalRequest = error.config
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+      const accessToken = await refreshAccessToken()
+      const user = JSON.parse(localStorage.getItem('user') as string)
+      user.accessToken = accessToken
+      localStorage.setItem('user', JSON.stringify(user))
+      http.defaults.headers.common.Authorization = `Bearer ${accessToken}`
+      return http(originalRequest)
+    }
+    return Promise.reject(error)
+  },
+)
 
 export default http
